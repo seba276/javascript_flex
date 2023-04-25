1e8fdffba0408d77a24c4688b7d815ef0d93f9f1
seba276 <sebastian.cidade@gmail.com>
2023-04-24 22:03:20 -0300

    Primer Subida

---
+++ js/script.js
@@ -0,0 +1,13 @@
+let nombre = prompt("¿Cual es tu nombre?")
+let apellido = prompt("¿Cual es tu apellido?")
+
+
+
+let saludoCliente = 'Hola! ' + nombre + ' ' + apellido
+
+alert(saludoCliente)
+let edad = prompt("¿Cuantos Años Tenes?")
+let nacimiento = 2023 - edad
+let cuandoNaciste = nombre + " " + apellido + (" naciste en el año ") + nacimiento
+
+alert(cuandoNaciste)
